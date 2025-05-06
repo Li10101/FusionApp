d00fd6291fb6d0d681d659a48c24798b2808ff6b
LiZhen <475002128@qq.com>
2025-05-06 14:10:49 +0800

    增加登录页布局，增加常用颜色值

--- app/src/main/java/com/fusion/SplashActivity.kt
+++ app/src/main/java/com/fusion/SplashActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //https://blog.csdn.net/guolin_blog/article/details/141500877
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +17,6 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
